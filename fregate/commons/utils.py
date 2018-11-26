07721a6bcf7cc8c77b1202e5ce3dc2cd02f74365
tonychg <antoine.chiny@gmail.com>
2018-11-26 12:29:12 +0100

    Add commons module

---
+++ fregate/commons/utils.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# =============================================================================
+# Name     : utils.py
+# Function :
+# Usage    :
+# Version  : 1.0.0
+# vi       : set expandtab shiftwidth=4 softtabstop=4
+# =============================================================================
